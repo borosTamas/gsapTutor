195cbd6fad4305edd55010a155237458353efd7a
borosTamas <ikuto19930802@gmail.com>
2019-08-15 20:00:43 +0200

    size hotfix

--- js/girlAnimation.js
+++ js/girlAnimation.js
@@ -8,8 +8,8 @@ const background = new TimelineMax({});
 const arm = new TimelineMax({paused:true, reversed: true});
 
 background.from(cape,1,{scaleY:0.75, repeat:-1, yoyo:true});
-TweenMax.to(clouds, 5, {x: "200%", repeat:-1, delay:1}, -0.75);
-TweenMax.staggerTo(ground, 10, {x: "1000%", repeat:-1, delay:2,  ease: Power0.easeNone}, -1);
+TweenMax.to(clouds, 10, {x: "400%", repeat:-1, delay:1}, -0.75);
+TweenMax.staggerTo(ground, 10, {x: "2500%", repeat:-1, delay:2,  ease: Power0.easeNone}, -1);
 
 
 girl.addEventListener("mouseenter",function () {
